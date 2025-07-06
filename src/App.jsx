2ead2bad54665f0ef847b0acaf8f7a183d21f0b4
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-07-06 19:01:33 +0530

    Animeitem and popular page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Popular from './components/Popular'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AnimeItem from './components/AnimeItem';
+import Gallery from './components/Gallery';
+import Popular from './components/Popular';
 
 function App() {
-  
-
   return (
-    <div>
-      hello world
-      <Popular />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Popular />} />
+        <Route path="/anime/:id" element={<AnimeItem />} />
+        <Route path="/character/:id" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import GlobalStyle from './Globalstyle.jsx'
-import { GlobalContextProvider } from './context/global.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { GlobalContextProvider } from "./context/global.jsx";
+import GlobalStyle from "./Globalstyle.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalStyle />
     <GlobalContextProvider>
+      <GlobalStyle />
       <App />
     </GlobalContextProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
